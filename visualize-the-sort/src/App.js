82e637b11d394e50944f8b2d75aeff9d487b82e8
Sumit <sumit22mittal@gmail.com>
2020-06-04 22:37:12 -0400

    updated app.js

--- visualize-the-sort/src/App.js
+++ visualize-the-sort/src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+
+        </div>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
